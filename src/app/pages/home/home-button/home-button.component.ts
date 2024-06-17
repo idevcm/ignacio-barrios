7c979e613f20b56c688b5c7fcbb839b36fc48254
Ignacio Javier Barrios Hernández <ignarrios@gmail.com>
2024-06-17 16:14:26 +0100

    feat: Setup home page with load spinner and header

---
+++ src/app/pages/home/home-button/home-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-button.component.html',
+  styleUrl: './home-button.component.scss'
+})
+export class HomeButtonComponent {
+
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {HeaderComponent} from "../../shared/header/header.component";
+import {LoadingSpinnerComponent} from "../../shared/loading-spinner/loading-spinner.component";
+import {HomeButtonComponent} from "./home-button/home-button.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    LoadingSpinnerComponent,
+    HomeButtonComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+  typedText = ''; // Variable para almacenar el texto que se está escribiendo y borrando
+  textArray = ["Ignacio Barrios", "Programador Backend", "Java Developer", "Angular Developer"];
+  typingDelay = 100;
+  erasingDelay = 50;
+  newTextDelay = 1500; // Delay entre el texto actual y el siguiente
+  textArrayIndex = 0;
+  charIndex = 0;
+
+  ngOnInit(): void {
+    if (this.textArray.length) setTimeout(() => this.type(), this.newTextDelay + 250);
+  }
+
+  type() {
+    if (this.charIndex < this.textArray[this.textArrayIndex].length) {
+      this.typedText += this.textArray[this.textArrayIndex].charAt(this.charIndex);
+      this.charIndex++;
+      setTimeout(() => this.type(), this.typingDelay);
+    } else {
+      setTimeout(() => this.erase(), this.newTextDelay);
+    }
+  }
+
+  erase() {
+    if (this.charIndex > 0) {
+      this.typedText = this.textArray[this.textArrayIndex].substring(0, this.charIndex - 1);
+      this.charIndex--;
+      setTimeout(() => this.erase(), this.erasingDelay);
+    } else {
+      this.textArrayIndex++;
+      if (this.textArrayIndex >= this.textArray.length) this.textArrayIndex = 0;
+      setTimeout(() => this.type(), this.typingDelay + 1100);
+    }
+  }
+}
+
